abd3f7f5ad522965d7f7109900f2f008f7e16006
Alstudd <alstonsoares17@gmail.com>
2024-01-31 09:32:45 +0530

    Added firebase config

---
+++ client/app/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8skiDFvBuBwy0K2tSRTPdIH1PUt7JN5E",
+  authDomain: "dream-avenue.firebaseapp.com",
+  projectId: "dream-avenue",
+  storageBucket: "dream-avenue.appspot.com",
+  messagingSenderId: "811881172636",
+  appId: "1:811881172636:web:80fd86591fe170233b2759"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+export const auth = getAuth(app)
+export const db = getFirestore(app);
